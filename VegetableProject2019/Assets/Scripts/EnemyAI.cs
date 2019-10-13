bf598ef75ff2140642b9cb565364e949deecb860
SemonT <56264177+SemonT@users.noreply.github.com>
2019-10-13 22:35:32 +0400

    9242

---
+++ VegetableProject2019/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EnemyAI : MonoBehaviour
+{
+    // Параметры
+    public GameObject target;
+
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float speed = 7f;
+    [SerializeField] float view_distance = 65f;
+
+    // Служебные переменные
+    Rigidbody2D m_rigidbody;
+    Animator m_animator;
+    Attack m_attack;
+
+
+    Transform m_RCSGroundTransform;
+
+    Transform m_TopEye;
+    Transform m_BottomEye;
+    Transform m_BackEye;
+
+    bool dir = false;
+    float dur = 1;
+    float jumpCooldown = 1f;
+    float jumpTimer = 0;
+
+    void Start()
+    {
+//      m_rigidbody = GetComponent<Rigidbody2D>();
+
+        m_animator = GetComponent<Animator>();
+        m_attack = GetComponent<Attack>();
+
+        Transform RCSParentTransform = transform.Find("RayCastSources");
+
+        m_TopEye = RCSParentTransform.Find("FrontEyesTop");
+        m_BottomEye = RCSParentTransform.Find("FrontEyesBottom");
+        m_BackEye = RCSParentTransform.Find("BackEyes");
+
+    }
+
+    // Вызывается каждый кадр с параметрами 
+    void MoveHorizontal(GameObject target)
+    {
+        float delta = transform.position.x - target.transform.position.x;
+
+        m_animator.SetFloat("Move", Mathf.Abs(delta));
+
+        print("Дельта " + Mathf.Abs(delta));
+
+        if(Mathf.Abs(delta) < 6f && Mathf.Abs(delta) > 3.3f)
+        {
+            print("Атакую ");
+            m_attack.input(true);
+        }
+        
+
+        if (delta < 0 && !dir || delta > 0 && dir)
+        {
+            dir = !dir;
+            transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        }
+      
+        Vector2 horiz_axis = new Vector2(target.transform.position.x, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, horiz_axis, speed * Time.deltaTime);
+        
+    }
+
+    GameObject FindTarget()
+    {
+        if (dir)
+            dur = -1f;
+        else
+            dur = 1f;
+
+        Physics2D.queriesHitTriggers = false;
+
+        Vector2 direction = new Vector2(m_TopEye.position.x - view_distance * dur, m_TopEye.position.y - 9f);
+        Debug.DrawLine(m_TopEye.position, direction, Color.red, Time.deltaTime);
+        RaycastHit2D hitTop = Physics2D.Raycast(m_TopEye.position, direction, view_distance);
+
+        direction = new Vector2(m_BottomEye.position.x - view_distance * dur, m_BottomEye.position.y + 9f);
+        Debug.DrawLine(m_BottomEye.position, direction, Color.blue, Time.deltaTime);
+        RaycastHit2D hitBottom = Physics2D.Raycast(m_TopEye.position, direction, view_distance);
+
+        direction = new Vector2(m_BackEye.position.x + (view_distance * 0.3f * dur), m_BackEye.position.y);
+        Debug.DrawLine(m_BackEye.position, direction, Color.green, Time.deltaTime);
+        RaycastHit2D hitBack = Physics2D.Raycast(m_BackEye.position, direction, view_distance * 0.3f);
+
+        
+
+
+        if (hitTop.collider != null && hitTop.collider.gameObject != null && hitTop.collider.gameObject.GetComponent<Player>() != null)
+        {
+            print("Найдено Верхним глазом " + hitTop.collider.gameObject.name);
+            return hitTop.collider.gameObject;
+        }
+
+        if (hitTop.collider != null && hitBottom.collider.gameObject != null && hitBottom.collider.gameObject.GetComponent<Player>() != null)
+        {
+            print("Найдено Нижним глазом " + hitBottom.collider.gameObject.name);
+            return hitBottom.collider.gameObject;
+        }
+
+        if (hitBack.collider != null && hitBack.collider.gameObject != null && hitBack.collider.gameObject.GetComponent<Player>() != null)
+        {
+            print("Найдено жеппой " + hitBack.collider.gameObject.name);
+            return hitBack.collider.gameObject;
+        }
+
+        print("Не Найдено");
+        return transform.gameObject;
+    }
+
+
+
+    private void Update()
+    {
+
+        m_attack.input(false);
+
+        MoveHorizontal(FindTarget());
+
+        
+
+    }
+
+
+
+}
---
+++ VegetableProject2019/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+    // Параметры
+    public Camera mainCamera;
+    public GameObject bleedPrefab;
+    public GameObject piesesPrefab;
+
+    // Служеблые переменные
+    Player player; // Скрипт игрока
+
+    // Вызов при старте
+    void Start()
+    {
+        player = Player.GetInstance();
+        Health.SetPrefabs(bleedPrefab, piesesPrefab);
+    }
+
+    // Вызов каждый кадр
+    void Update()
+    {
+        // Преследование камеры
+        mainCamera.transform.position = new Vector3(player.gameObject.transform.position.x, player.gameObject.transform.position.y, -10);
+        // Передача параметров ввода игровому персонажу
+        player.input(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetKeyDown(KeyCode.Space));
+    }
+}
