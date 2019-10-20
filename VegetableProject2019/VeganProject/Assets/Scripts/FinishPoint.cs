6e5ffb661f9d2be47553b7dec4d97263bb199325
SemonT <56264177+SemonT@users.noreply.github.com>
2019-10-20 23:19:23 +0400

    4648

---
+++ VegetableProject2019/VeganProject/Assets/Scripts/FinishPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishPoint : MonoBehaviour
+{
+    // Параметры
+    public string nextSceneName = "MainMenu";
+
+
+    // При попадании игрока
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == Player.GetInstance().gameObject)
+        {
+            GameSystem.GetInstance().LoadLevel(nextSceneName);
+        }
+    }
+}
---
+++ VegetableProject2019/VeganProject/Assets/Scripts/Weapon.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Параметры
+    public Transform handle;
+    public DamageDealer[] damageDealers;
+    public string WeaponType;
+
+    // Служебные переменные
+    private bool m_isAttacking;
+    public bool IsAttacking
+    {
+        set
+        {
+            m_isAttacking = value;
+            SetDamageDealers(value);
+        }
+        get {
+            return m_isAttacking;
+        }
+    }
+    private bool m_stunTrigger;
+    public bool StunTrigger
+    {
+        private set
+        {
+            m_stunTrigger = value;
+        }
+        get
+        {
+            bool t = m_stunTrigger;
+            m_stunTrigger = false;
+            return t;
+        }
+    }
+    [HideInInspector] public bool isPicked = false;
+
+    // Установка всех наносителей урона
+    void SetDamageDealers(bool state)
+    {
+        foreach (DamageDealer dd in damageDealers)
+        {
+            dd.isActive = state;
+        }
+    }
+
+    // Вызывается множество раз при атаке
+    public void Input()
+    {
+        foreach (DamageDealer dd in damageDealers)
+        {
+            if (!dd.isActive) {
+                SetDamageDealers(false);
+                if (dd.StunTrigger)
+                {
+                    StunTrigger = true;
+                }
+                break;
+            }
+        }
+    }
+
+    // Получение типа оружия для определения анимации
+    public string GetWeaponType()
+    {
+        return WeaponType;
+    }
+}
